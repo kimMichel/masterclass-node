e3589e3d00a2e7702ef3785e9a74537f4990d542
Michel Kim <kim98.michel@gmail.com>
2023-09-08 11:02:35 -0300

    create the server and configure lint

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
-const nome: string = 'Michel'
+import fastify from 'fastify'
 
-console.log(nome)
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello World'
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
